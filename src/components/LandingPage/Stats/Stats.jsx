a8f705b7070106cec2634f26e452ebd69aef26d9
MariamO6274 <ostatishvili@gmail.com>
2024-04-11 14:03:59 -0600

    general and stats style

--- src/components/LandingPage/Stats/Stats.jsx
+++ src/components/LandingPage/Stats/Stats.jsx
@@ -1,8 +1,9 @@
 import { stats } from "../../../../src/constants";
+import styles from "../../../style";
 
 
 const Stats = () => (
-  <section className={` flex-row flex-wrap sm:mb-20 mb-6`}>
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
     {stats.map((stat) => (
       <div
         key={stat.id}
